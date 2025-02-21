b1d68f1e5ee325597d8d212b53c88762db9b3897
Marinel <alexei.dulgher1@gmail.com>
2025-02-21 18:12:46 -0500

    start of tp3

---
+++ TP3/Employe.cpp
@@ -0,0 +1,28 @@
+#include "Employe.h"
+
+// TODO: Implémenter le constructeur de Employe.
+// - Initialiser le nom de l'employé avec la valeur passée en paramètre.
+Employe::Employe(const string& nom) : nom_(nom) {}
+
+// TODO: Implémenter la méthode getNom.
+// - Retourner le nom de l'employé.
+string Employe::getNom() const {
+    return nom_;
+}
+
+// TODO: Implémenter la méthode setNom.
+// - Mettre à jour le nom de l'employé avec la valeur passée en paramètre.
+void Employe::setNom(const string& nom) {
+    nom_ = nom;
+}
+
+ostream& Employe::afficher(ostream &out) const {
+    out << "Nom: " << nom_ << endl;
+    return out;
+}
+
+// TODO: Surcharger l'opérateur << pour afficher les informations de l'employé.
+ostream &operator<<(ostream &out, const Employe &e) {
+    out << e.nom_;
+    return out;
+}
---
+++ TP3/Liste.h
@@ -0,0 +1,116 @@
+#pragma once
+#include <vector>
+#include "Medecin.h"
+#include <memory>
+using namespace std;
+
+template<typename T>
+class Liste
+{   
+public:
+    Liste(){};
+    ~Liste() = default;
+
+    // TODO: Surcharger l'opérateur += pour ajouter un élément à la liste.
+    // Vérifier que l'élément n'existe pas déjà avant de l'ajouter.
+    Liste& operator+=(const T& obj) {
+        // TODO: Implémenter la logique d'ajout d'un élément.
+        for (T& element : listes_){
+            if (element == obj) {
+                return *this;
+            }
+        }
+        listes_.push_back(obj);
+        return *this;
+    }
+
+    // TODO: Surcharger l'opérateur += pour ajouter tous les éléments d'une autre liste.
+    // Vérifier que les éléments n'existent pas déjà avant de les ajouter.
+    Liste& operator+=(const Liste<T>& liste) {
+        // TODO: Implémenter la logique d'ajout d'une liste.
+        for (T& elemAutre : liste) {
+            for (T& elem : listes_) {
+                if (elemAutre == elem) {
+                    break;
+                }
+            }
+            listes_.push_back(elemAutre);
+        }
+        return *this;
+    }
+
+    // TODO: Surcharger l'opérateur -= pour retirer un élément de la liste.
+    // Utiliser la méthode chercher pour trouver l'élément à retirer.
+    Liste& operator-=(const T& obj) {
+        // TODO: Implémenter la logique de suppression d'un élément.
+        int index = chercher(obj);
+        if (index != -1) {
+            listes_.erase(index);
+        }
+        return *this;
+    }
+
+    // TODO: Surcharger l'opérateur -= pour retirer un élément par son nom.
+    // Utiliser la méthode chercher pour trouver l'élément à retirer.
+    Liste& operator-=(const string& nom) {
+        // TODO: Implémenter la logique de suppression par nom.
+        int index = chercher(nom);
+        if (index != -1) {
+            listes_.erase(index);
+        }
+        return *this;
+    }
+
+    // TODO: Surcharger l'opérateur -= pour retirer tous les éléments d'une autre liste.
+    // Utiliser la méthode chercher pour vérifier l'existence des éléments.
+    Liste& operator-=(const Liste<T>& liste) {
+        // TODO: Implémenter la logique de suppression d'une liste.
+        for (T& elem : liste) {
+            listes -= elem;
+        }
+        return *this;
+    }
+
+    // TODO: Surcharger l'opérateur [] pour accéder à un élément de la liste par index.
+    // Retourner une référence constante à l'élément.
+    const T& operator [](int index) const{
+        return listes_[index];
+    }
+
+    // TODO: Surcharger l'opérateur [] pour accéder à un élément de la liste par index.
+    // Retourner une référence modifiable à l'élément.
+    T& operator [](int index) {
+        return listes_[index];
+    }
+
+    // TODO: Implémenter la méthode chercher pour trouver un élément par son nom.
+    // Retourner l'index de l'élément s'il est trouvé, sinon retourner -1.
+    int chercher(const string& nom) const {
+        // TODO: Implémenter la recherche par nom.
+        for (int i = 0; i < listes_.size(); i++) {
+            if (listes_[i].nom_ == nom) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // TODO: Implémenter la méthode chercher pour trouver un élément par comparaison directe.
+    // Retourner l'index de l'élément s'il est trouvé, sinon retourner -1.
+    int chercher(const T& obj) const {
+        for (int i = 0; i < listes_.size(); i++) {
+            if (listes_[i] == obj) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // TODO: Implémenter la méthode size pour retourner la taille de la liste.
+    size_t size() const {
+        return listes_.size();
+    }
+
+private:
+    vector<T> listes_;
+};
